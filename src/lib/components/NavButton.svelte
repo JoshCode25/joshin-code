34d7c1d840159a61e98f7b8b6b6ee2956f03e452
JoshG <joshcode25@gmail.com>
2023-03-06 06:39:05 -0600

    added NavButton for hoverDropdown

--- src/lib/components/NavButton.svelte
+++ src/lib/components/NavButton.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
 
+	interface ButtonInfo {
+		buttonName: string;
+		buttonUrl: string;
+		displayInfo: Array<DisplayInfo>;
+	}
 	interface DisplayInfo {
 		url: string;
 		name: string;
 	}
 
-	export let buttonName: string;
-	export let buttonUrl: string = '/';
-	export let displayInfo: Array<DisplayInfo>;
+	export let buttonInfo: ButtonInfo = {
+		buttonName: 'default',
+		buttonUrl: '/',
+		displayInfo: [{ url: '/', name: 'Home' }]
+	};
 
 	let isDisplayOpen: boolean = true;
+	let clientLeft: number;
+	$: console.log(clientLeft);
 
 	function openDisplay() {
 		isDisplayOpen = true;
@@ -21,15 +30,44 @@
 	}
 </script>
 
-<a href={buttonUrl} on:mouseenter={openDisplay} on:mouseleave={closeDisplay}>
-	{buttonName}
+<a
+	bind:offsetWidth={clientLeft}
+	href={buttonInfo.buttonUrl}
+	on:mouseenter={openDisplay}
+	on:mouseleave={closeDisplay}
+>
+	{buttonInfo.buttonName}
 </a>
 {#if isDisplayOpen}
 	<ul class="projectSlide" on:mouseenter={openDisplay} on:mouseleave={closeDisplay}>
-		{#each displayInfo as info}
+		{#each buttonInfo.displayInfo as info}
 			<li transition:slide>
 				<a href={info.url}>{info.name}</a>
 			</li>
 		{/each}
 	</ul>
 {/if}
+
+<style>
+	a {
+		color: var(--primary-color);
+		background: var(--primary-dark);
+		display: inline-block;
+		padding: 20px;
+		font-size: 2.5rem;
+		font-weight: bold;
+	}
+	a:hover {
+		color: var(--secondary-color);
+		cursor: pointer;
+	}
+	li > a {
+		width: 100%;
+	}
+	.projectSlide {
+		position: absolute;
+		top: 68.8;
+		width: 200px;
+		text-align: left;
+	}
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import { allProjectNames } from '$lib/stores/pageInfo';
 	import { slide } from 'svelte/transition';
+	import NavButton from '$lib/components/NavButton.svelte';
+
+	const projectsDisplayInfo = {
+		buttonName: 'Projects',
+		buttonUrl: '/projects',
+		displayInfo: $allProjectNames
+	};
 
 	let isDisplayingProjects: boolean = true;
 
@@ -16,25 +23,7 @@
 <header>
 	<a href={`/about/`}>About</a>
 	<a href={`/`}>Home</a>
-	<a
-		href={`/projects/`}
-		on:mouseenter={openDisplayingProjects}
-		on:mouseleave={closeDisplayingProjects}
-		>Projects
-	</a>
-	{#if isDisplayingProjects}
-		<ul
-			class="projectSlide"
-			on:mouseenter={openDisplayingProjects}
-			on:mouseleave={closeDisplayingProjects}
-		>
-			{#each $allProjectNames as project}
-				<li transition:slide>
-					<a href={`/projects/${project[1]}`}>{project[0]}</a>
-				</li>
-			{/each}
-		</ul>
-	{/if}
+	<NavButton buttonInfo={projectsDisplayInfo} />
 	<a href={`/contact/`}>Contact</a>
 </header>
 
@@ -54,11 +43,5 @@
 	a:hover {
 		color: var(--secondary-color);
 		cursor: pointer;
-	}
-	.projectSlide {
-		position: absolute;
-		top: 68.8;
-		width: 200px;
-		text-align: left;
 	}
 </style>
