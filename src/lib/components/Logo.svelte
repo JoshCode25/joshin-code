43f8b34957638cf2dc3f3677cd1cac519b052594
JoshG <joshcode25@gmail.com>
2023-03-07 06:34:04 -0600

    moved glow animation to global css file

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -17,26 +17,6 @@
 		font-size: 2rem;
 		color: var(--primary-light);
 	}
-	@keyframes glow {
-		0% {
-			color: white;
-		}
-		20% {
-			color: yellow;
-		}
-		40% {
-			color: orange;
-		}
-		60% {
-			color: red;
-		}
-		80% {
-			color: orange;
-		}
-		100% {
-			color: yellow;
-		}
-	}
 	span {
 		animation-name: glow;
 		animation-duration: 4s;
