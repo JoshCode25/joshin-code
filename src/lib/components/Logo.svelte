73e89d5249d7b9eaf4973c22d55021d5bdbed312
JoshG <joshcode25@gmail.com>
2023-03-09 21:52:01 -0600

    added click functionality to Logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,25 +1,50 @@
-<div>
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+
+	const displayList: Array<string> = ['josh', 'figures', 'trees', 'land', 'flocks', 'cities'];
+	let displayNumber = 0;
+	const maxDisplayAdvance = 3;
+	let displayName: string;
+	$: displayName = displayList[displayNumber];
+
+	function toggleDisplayName(): void {
+		let displayAdvance: number = Math.floor(Math.random() * maxDisplayAdvance);
+		if (displayNumber + displayAdvance > displayList.length - 1) {
+			displayNumber = displayNumber + displayAdvance - displayList.length;
+		} else {
+			displayNumber = displayNumber + displayAdvance;
+		}
+	}
+</script>
+
+<button class="reset" on:click={toggleDisplayName}>
 	<h3>developing</h3>
 	<h4>
-		<span>{'<'}</span>
-		josh
-		<span>{'/>'}</span>
+		<span class="glow">{'<'}</span>
+		{#key displayName}
+			<span class="inlineBlock" in:slide>{displayName}</span>
+		{/key}
+		<span class="glow">{'/>'}</span>
 	</h4>
-</div>
+</button>
 
 <style>
-	div {
+	button {
 		margin: 1rem 2rem;
 		text-align: left;
 	}
+
 	h3,
 	h4 {
 		font-size: 2rem;
 		color: var(--primary-light);
 	}
-	span {
+	.glow {
 		animation-name: glow;
 		animation-duration: 4s;
 		animation-iteration-count: infinite;
+	}
+	.inlineBlock {
+		display: inline-block;
 	}
 </style>
